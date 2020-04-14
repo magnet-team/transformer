421c60764c4f3193637f37a4f920e1e888fdba8c
Martin Streicher <martin.streicher@gmail.com>
2020-04-14 11:30:27 -0400

    Update name of workflow

--- lib/transformer/path/collection.rb
+++ lib/transformer/path/collection.rb
@@ -7,10 +7,15 @@ module Transformer
         modified_mapping = mapping.first.deep_dup
         source           = modified_mapping.delete('source')
         updated_path     = "#{path}.#{source}[*]"
+        elements         = JsonPath.new(updated_path).on(json)
 
-        modified_mapping.each_with_object({}) do |(key, value), result|
-          transformer   = Factory.manufacture(mapping: value, path: updated_path)
-          result[key] = transformer.transform(json)
+        elements.each_with_index.map do |element, index|
+          updated_path = "#{path}.#{source}[#{index}]"
+
+          modified_mapping.each_with_object({}) do |(key, value), result|
+            transformer = Factory.manufacture(mapping: value || key, path: updated_path)
+            result[key] = transformer.transform(json)
+          end
         end
       end
     end
